9ad82281bd19ffc788b12288409a34796267eb41
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-09-24 10:10:01 +0200

    add: eslint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,15 @@ const expoConfig = require("eslint-config-expo/flat");
 const eslintPluginPrettierRecommended = require("eslint-plugin-prettier/recommended");
 const eslintPluginReactNative = require("eslint-plugin-react-native");
 
+// Import the FlatCompat class
+const { FlatCompat } = require("@eslint/eslintrc");
+const path = require("path");
+
+// Create a compat object with the current directory as the base
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
 module.exports = defineConfig([
   expoConfig,
   eslintPluginPrettierRecommended,
@@ -14,4 +23,19 @@ module.exports = defineConfig([
       "react-native/no-unused-styles": "error",
     },
   },
+  // Use the compat object to convert the old-style import config
+  ...compat.config({
+    plugins: ["import"],
+    settings: {
+      "import/resolver": {
+        alias: {
+          map: [["@", "./"]],
+          extensions: [".js", ".jsx"],
+        },
+      },
+    },
+    rules: {
+      "import/no-unresolved": "error",
+    },
+  }),
 ]);
