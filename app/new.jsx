0b1384b7a49da8a7018ec050b044af12454b1b93
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-09-24 12:13:59 +0200

    add: new plant form

--- app/new.jsx
+++ app/new.jsx
@@ -1,10 +1,61 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Alert, TextInput } from "react-native";
 import { theme } from "@/theme";
+import { useState } from "react";
+import { PlantlyImage } from "@/components/PlantlyImage";
+import { PlantlyButton } from "@/components/PlantlyButton";
 
 export default function NewScreen() {
+  const [name, setName] = useState("");
+  const [days, setDays] = useState("");
+
+  const handleSubmit = () => {
+    if (!name) {
+      return Alert.alert(
+        "Validation error!",
+        "You must give a name to your plant"
+      );
+    }
+
+    if (!days) {
+      return Alert.alert(
+        "Validation error",
+        `How does often ${name} need hydration`
+      );
+    }
+
+    if (Number.isNaN(Number(days))) {
+      return Alert.alert(
+        "Validation error",
+        "Watering frequency must be a number"
+      );
+    }
+
+    console.log("Adding a plant", name, days);
+  };
   return (
     <View style={styles.container}>
-      <Text>New plant</Text>
+      <View style={styles.centered}>
+        <View style={styles.imageContainer}>
+          <PlantlyImage />
+        </View>
+        <View style={styles.formContainer}>
+          <Text style={styles.label}>Name</Text>
+          <TextInput
+            placeholder="E.g. Casper the Cactus"
+            value={name}
+            onChangeText={setName}
+            style={styles.input}
+          />
+          <Text style={styles.label}>Watering Frequency (every x days)</Text>
+          <TextInput
+            placeholder="E.g. 6"
+            value={days}
+            onChangeText={setDays}
+            style={styles.input}
+          ></TextInput>
+          <PlantlyButton title={"Add plant"} onPress={handleSubmit} />
+        </View>
+      </View>
     </View>
   );
 }
@@ -14,6 +65,26 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: theme.colorWhite,
     justifyContent: "center",
+    alignItems: "center",
+  },
+  formContainer: {
+    paddingTop: 24,
+    paddingHorizontal: 24,
+    paddingBottom: 100,
+  },
+  input: {
+    borderWidth: 2,
+    borderColor: theme.colorLightGrey,
+    padding: 12,
+    borderRadius: 6,
+    marginBottom: 24,
+    fontSize: 18,
+  },
+  label: {
+    fontSize: 18,
+    marginBottom: 0,
+  },
+  centered: {
     alignItems: "center",
   },
 });
