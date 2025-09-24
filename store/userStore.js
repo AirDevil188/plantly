c5ee480541f934cab861fac6b93e8e850a10f0a8
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-09-24 10:25:19 +0200

    add asyncStorage to state managment for persisted state

--- store/userStore.js
+++ store/userStore.js
@@ -1,13 +1,23 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
 
-export const useUserStore = create((set) => ({
-  hasFinishedOnboarding: false,
-  toggleHasOnboarded: () => {
-    set((state) => {
-      return {
-        ...state,
-        hasFinishedOnboarding: !state.hasFinishedOnboarding,
-      };
-    });
-  },
-}));
+export const useUserStore = create(
+  persist(
+    (set) => ({
+      hasFinishedOnboarding: false,
+      toggleHasOnboarded: () => {
+        set((state) => {
+          return {
+            ...state,
+            hasFinishedOnboarding: !state.hasFinishedOnboarding,
+          };
+        });
+      },
+    }),
+    {
+      name: "plantly-user-store",
+      storage: createJSONStorage(() => AsyncStorage),
+    }
+  )
+);
