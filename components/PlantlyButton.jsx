e9c1cdbe9c6429515194c504f8beab08c103a473
AirDevil188 <90569889+AirDevil188@users.noreply.github.com>
2025-09-24 10:25:57 +0200

    add: PlantlyButton component

---
+++ components/PlantlyButton.jsx
@@ -0,0 +1,24 @@
+import { theme } from "@/theme";
+import { StyleSheet, Text, Pressable } from "react-native";
+
+export function PlantlyButton({ title, onPress }) {
+  return (
+    <Pressable onPress={onPress} style={styles.button}>
+      <Text style={styles.text}>{title}</Text>
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({
+  text: {
+    color: "white",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  button: {
+    paddingHorizontal: 18,
+    paddingVertical: 12,
+    borderRadius: 6,
+    backgroundColor: theme.colorGreen,
+  },
+});
